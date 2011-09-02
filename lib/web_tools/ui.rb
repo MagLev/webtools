158a20a1a6ee45a467b881a4976116949a4c39f7
Tim Felgentreff <timfelgentreff@gmail.com>
2011-09-01 17:46:30 -0700

    create JS and CSS paths in UI

--- lib/web_tools/ui.rb
+++ lib/web_tools/ui.rb
@@ -12,25 +12,62 @@ class WebTools::UI < Sinatra::Base
 
   before do
     @location = env["SCRIPT_NAME"]
-    @scripts = []
+    @javascripts = %w[
+                     https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+                     https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js
+                   ]
+    @stylesheets = %w[reset.css webtools.css]
   end
 
   get '/browser' do
-    @scripts += %w[webtools/browser CodeMirror/js/codemirror]
+    @javascripts += %w[webtools/browser.js CodeMirror/js/codemirror.js]
     erb :browser
   end
 
   get '/debugger' do
-    "todo"
+    @stylesheets += %w[
+      http://alexgorbatchev.com/pub/sh/current/styles/shCore.css
+      http://alexgorbatchev.com/pub/sh/current/styles/shThemeDefault.css
+      stylesheets/webtools-debugger.css]
+    @javascripts += %w[
+      https://raw.github.com/alexgorbatchev/SyntaxHighlighter/master/scripts/XRegExp.js
+      https://raw.github.com/alexgorbatchev/SyntaxHighlighter/master/scripts/shCore.js
+      http://alexgorbatchev.com/pub/sh/current/scripts/shBrushRuby.js
+      webtools/debugger.js]
+    erb :debugger
   end
 
   get '/info/version' do
-    @scripts += %w[webtools/version]
+    @javascripts += %w[webtools/version.js]
+    @stylesheets = []
     erb :version
   end
 
   get '/info/sessions' do
-    @scripts += %w[webtools/sessions]
+    @javascripts += %w[webtools/sessions.js]
+    @stylesheets = []
     erb :sessions
+  end
+
+  private
+
+  def erb(*args, &block)
+    @stylesheets = @stylesheets.map do |path|
+      unless path =~ /^((http)|\/)/
+        "#{@location}/stylesheets/#{path}"
+      else
+        path
+      end
+    end
+
+    @javascripts = @javascripts.map do |path|
+      p path
+      unless path =~ /^((http)|\/)/
+        "#{@location}/javascript/#{path}"
+      else
+        path
+      end
+    end
+    super
   end
 end
