b624ac62590fe9a8bd4c2a243974a01ca586f534
Tim Felgentreff <timfelgentreff@gmail.com>
2011-10-25 11:36:10 -0700

    split logic, maglev can be more advanced here

--- lib/web_tools/workspace.rb
+++ lib/web_tools/workspace.rb
@@ -15,32 +15,9 @@ module WebTools
 
     post '/evaluate' do
       response = nil
-      client = Thread.start do
-        begin
-          value = eval(params["text"])
-          result = { "klass" => value.class.inspect,
-            "string" => value.inspect }
-          if value.is_a? Module
-            result["dict"] = ""
-            result["name"] = value.inspect
-            result["cat"]  = ""
-          end
-          response = json(result)
-        rescue SyntaxError => e
-          response = json("errorType" => "compileError",
-                          "errorDetails" => [[1031, 1, e.message, nil, nil]])
-        rescue Exception => e
-          entry = Support::ErrorLog.add :thread => Thread.current,
-                                        :exception => e
-          response = json("errorType" => entry.exception.class.inspect,
-                          "description" => entry.exception.message,
-                          "oop" => entry.object_id)
-          Thread.stop
-        end
+      debug_on_exception do
+        run_evaluation params["text"]
       end
-
-      sleep 0.2 until client.stop?
-      response
     end
 
     post '/saveMethod' do
@@ -61,5 +38,54 @@ module WebTools
         return json("compileError" => [[1031, 1, e.message, nil, nil]])
       end
     end
+
+    if defined? Maglev
+      require 'maglev/debugger'
+      # Run block under the maglev debugger, that saves a continuation
+      # to the object log
+      def debug_on_exception(&block)
+        Maglev::Debugger.debug(&block)
+      rescue Exception => e
+        json("errorType" => e.class.inspect,
+             "description" => e.message,
+             "oop" => e.log_entry.continuation.object_id)
+      end
+    else
+      # Run block in a separate thread, which gets suspended if an
+      # error occurs.
+      def debug_on_exception
+        response = nil
+        client = Thread.start do
+          begin
+            yield
+          rescue Exception => e
+            entry = Support::ErrorLog.add :thread => Thread.current,
+                                          :exception => e
+            response = json("errorType" => entry.exception.class.inspect,
+                            "description" => entry.exception.message,
+                            "oop" => entry.object_id)
+            Thread.stop
+          end
+        end
+        sleep 0.2 until client.stop?
+        response
+      end
+    end
+
+    def run_evaluation(text)
+      value = eval(params["text"])
+      result = { "klass" => value.class.inspect,
+        "string" => value.inspect }
+      if value.is_a? Module
+        result["dict"] = ""
+        result["name"] = value.inspect
+        result["cat"]  = ""
+      end
+      json(result)
+    rescue SyntaxError => e
+      json("errorType" => "compileError",
+           "errorDetails" => [[1031, 1, e.message, nil, nil]])
+    end
+
   end
 end
